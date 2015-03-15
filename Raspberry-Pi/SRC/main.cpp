6a9a81fc631eafb2203762dc265ee2e0e7682b14
RyodoTanaka <k104073r@mail.kyutech.jp>
2015-03-15 19:29:11 +0900

    [modify] カメラ機能以外のソースを削除
    コンパイルをするようにはしているが、使ってはいない

--- Raspberry-Pi/SRC/main.cpp
+++ Raspberry-Pi/SRC/main.cpp
@@ -1,28 +1,21 @@
+#include <wiringPi.h>
 #include <iostream>
-#include <unistd.h>
-#include <cstdio>
-#include <sstream>
-#include <exception>
 #include "main.hpp"
-
 
 using namespace std;
 
 int main()
 {
-  try {
-    Motor motor;  
-    
-    for(float i=1.0; i>=-1.0; i-=0.01){
-      motor.Drive(1.0,i);
-      usleep(100000);
-    }
-
-  }catch(exception &e){
-    cerr << e.what() << endl;
-    cerr << "Stop the Program..." << endl;
-    exit(1);
+  // --------------------------------
+  // Camera Section 
+  // (It has infinity loop)
+  // --------------------------------
+  RaspiCamera camera(320,240,0,0,0);
+  while(cvWaitKey(1) != 27){
+    camera.img = raspiCamCvQueryFrame(camera.cap);
+    cvShowImage("RasPi Camera", camera.img);
   }
-
+  // --------------------------------
+  
   return 0 ;
 }
