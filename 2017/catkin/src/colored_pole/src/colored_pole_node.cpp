436b006dabc0569ad7bb4e9c47771c52be131877
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-24 00:42:52 +0900

    Add color presets to colored_pole class
    本当は鎮火，消火，壁，みたいな名前にしたかったが
    どうも長い上に統一感がない英単語しか浮かばかなったので，もう単純に色名で
    
    多分プロジェクト内でも色でしか呼ばないだろうし，
    colored_poleというクラス名に合致しているのでまあ良かろう

--- 2017/catkin/src/colored_pole/src/colored_pole_node.cpp
+++ 2017/catkin/src/colored_pole/src/colored_pole_node.cpp
@@ -1,4 +1,24 @@
+#include <tuple>
+#include <vector>
+
 #include <ros/ros.h>
+
+
+namespace rcr {
+
+
+class colored_pole
+{
+public:
+  using color_type = std::tuple<int,int,int>;
+
+  static constexpr color_type red    {0xff, 0x00, 0x00};
+  static constexpr color_type yellow {0xff, 0xff, 0x00};
+  static constexpr color_type blue   {0x00, 0x00, 0xff};
+};
+
+
+} // namespace rcr
 
 
 int main(int argc, char** argv)
