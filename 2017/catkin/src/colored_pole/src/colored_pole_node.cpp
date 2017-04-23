5950a496a9f7c26b545443fa077389fbda91a4f8
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-24 01:16:49 +0900

    Add range check
    ウィンドウサイズの変数は後でrosparamに変更して，シミュレータから値を受け取るようにする

--- 2017/catkin/src/colored_pole/src/colored_pole_node.cpp
+++ 2017/catkin/src/colored_pole/src/colored_pole_node.cpp
@@ -18,13 +18,25 @@ public:
   static constexpr color_type yellow {0xff, 0xff, 0x00};
   static constexpr color_type blue   {0x00, 0x00, 0xff};
 
+  static constexpr double window_width  {500};
+  static constexpr double window_height {500};
+
 private:
   const coord_type coord_;
 
 public:
-  colored_pole(decltype(coord_)&& coord)
-    : coord_ {coord}
+  colored_pole(coord_type&& coord)
+    : coord_ {range_check(std::forward<decltype(coord)>(coord))}
   {}
+
+private:
+  static auto range_check(coord_type&& coord)
+    -> coord_type
+  {
+    coord.first = (coord.first < 0 ? 0 : (window_width < coord.first ? window_width : coord.first));
+    coord.second = (coord.second < 0 ? 0 : (window_height < coord.second ? window_height : coord.second));
+    return std::forward<decltype(coord)>(coord);
+  }
 };
 
 
