b91cc4204142e8848d242cb5591dfe99d6aa5260
YamaCIR-KIT <httperror@404-notfound.jp>
2017-04-24 00:57:34 +0900

    Update to treat coordinate

--- 2017/catkin/src/colored_pole/src/colored_pole_node.cpp
+++ 2017/catkin/src/colored_pole/src/colored_pole_node.cpp
@@ -1,4 +1,5 @@
 #include <tuple>
+#include <utility>
 #include <vector>
 
 #include <ros/ros.h>
@@ -11,10 +12,19 @@ class colored_pole
 {
 public:
   using color_type = std::tuple<int,int,int>;
+  using coord_type = std::pair<double, double>;
 
   static constexpr color_type red    {0xff, 0x00, 0x00};
   static constexpr color_type yellow {0xff, 0xff, 0x00};
   static constexpr color_type blue   {0x00, 0x00, 0xff};
+
+private:
+  const coord_type coord_;
+
+public:
+  colored_pole(decltype(coord_)&& coord)
+    : coord_ {coord}
+  {}
 };
 
 
@@ -26,6 +36,11 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "colored_pole_node");
 
   ros::NodeHandle node_handle {"~"};
+
+  std::vector<rcr::colored_pole> poles {
+    {{100.0, 100.0}},
+    {{200.0, 200.0}}
+  };
 
   while (ros::ok())
   {
