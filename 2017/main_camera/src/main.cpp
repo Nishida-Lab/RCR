3e649b1e31ab60da3e803c91941171ef700fe974
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-09 06:52:41 +0900

    Update main.cpp

--- 2017/main_camera/src/main.cpp
+++ 2017/main_camera/src/main.cpp
@@ -9,18 +9,39 @@
 
 #include <main_camera/version.hpp>
 
+
 int main(int argc, char** argv)
 {
   raspicam::RaspiCam camera {};
 
   if (!camera.open())
   {
-    std::cerr << "[debug] failed to open camera\n";
+    std::cerr << "\e[0;31m" << "[debug] failed to open camera" << "\e[0;37m\n";
     std::exit(EXIT_FAILURE);
   }
 
   std::cout << "[debug] waiting for camera stabilize...\n";
   std::this_thread::sleep_for(std::chrono::seconds(3));
+
+  std::cout << "[debug] start image capture\n";
+  camera.grab();
+
+  std::cout << "[debug] memory allocationg\n";
+  unsigned char* data {new unsigned char [camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)]};
+
+  std::cout << "[debug] extract image in RGB format\n";
+  camera.retrieve(data, raspicam::RASPICAM_FORMAT_RGB);
+
+  std::string file_name {"test_image.ppm"};
+  std::ofstream ofs {file_name, std::ios::binary};
+
+  std::cout << "[debug] write image\n";
+  ofs << "P6\n" << camera.getWidth() << " " << camera.getHeight() << " 255\n";
+  ofs.write(reinterpret_cast<char*>(data), camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
+
+  std::cout << "[debug] image saved: " << file_name << std::endl;
+
+  delete data;
 
   return 0;
 }
