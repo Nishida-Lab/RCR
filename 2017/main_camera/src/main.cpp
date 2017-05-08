b6d856894589aa65932c91858829f4bbbd20a555
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-08 11:59:47 +0900

    Add missing link library "raspicam"

--- 2017/main_camera/src/main.cpp
+++ 2017/main_camera/src/main.cpp
@@ -1,11 +1,26 @@
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <thread>
+
+#include <raspicam/raspicam.h>
 
 #include <main_camera/version.hpp>
 
 int main(int argc, char** argv)
 {
-  std::cout << "[debug] camera program for robocar race 2017\n"
-            << "        version: " << project_version.data() << std::endl;
+  raspicam::RaspiCam camera {};
+
+  if (!camera.open())
+  {
+    std::cerr << "[debug] failed to open camera\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::cout << "[debug] waiting for camera stabilize...\n";
+  std::this_thread::sleep_for(std::chrono::seconds(3));
 
   return 0;
 }
