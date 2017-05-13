0a91d3b55064f5793ab8effc68293fc92792aeb7
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-13 21:10:21 +0900

    Add function "normalize"
    boostjp からパクってきたやつ

--- 2017/algorithm/src/main.cpp
+++ 2017/algorithm/src/main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/vector.hpp>
 
 #include <algorithm/version.hpp>
 
+
+template <typename T>
+auto normalize(const boost::numeric::ublas::vector<T>& v)
+  -> boost::numeric::ublas::vector<T>
+{
+  return v / boost::numeric::ublas::norm_2(v);
+}
+
+
 int main(int argc, char** argv)
 {
-  std::cout << "[debug] algorithm for robocar race 2017\n"
-            << "        version: " << project_version << std::endl;
+  const std::vector<std::vector<std::string>> world_map {
+    {{"F0"}, {"F1"}, {"F2"}, {"F3"}, {"F4"}, {"F5"}},
+    {{"E0"}, {"E1"}, {"E2"}, {"E3"}, {"E4"}, {"E5"}},
+    {{"D0"}, {"D1"}, {"D2"}, {"D3"}, {"D4"}, {"D5"}},
+    {{"C0"}, {"C1"}, {"C2"}, {"C3"}, {"C4"}, {"C5"}},
+    {{"B0"}, {"B1"}, {"B2"}, {"B3"}, {"B4"}, {"B5"}},
+    {{"A0"}, {"A1"}, {"A2"}, {"A3"}, {"A4"}, {"A5"}}
+  };
+
+  boost::numeric::ublas::vector<double> uv {2};
+
+  uv[0] = 1.0;
+  uv[1] = 0.0;
+
+  std::cout << "[debug] ublas vector: " << uv << std::endl;
 
   return 0;
 }
