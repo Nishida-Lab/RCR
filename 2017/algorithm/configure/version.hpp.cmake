6dbba521df0ad862e4938a5e0d8956be0d8737ca
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-13 20:39:47 +0900

    Add cmake configure files

---
+++ 2017/algorithm/configure/version.hpp.cmake
@@ -0,0 +1,14 @@
+#ifndef INCLUDED_ROBOCAR_2017_ALGORITHM_VERSION_HPP_
+#define INCLUDED_ROBOCAR_2017_ALGORITHM_VERSION_HPP_
+
+
+#include <string>
+
+const std::string project_version {"${PROJECT_VERSION}"};
+
+const std::string project_major_version {"${PROJECT_VERSION_MAJOR}"};
+const std::string project_minor_version {"${PROJECT_VERSION_MINOR}"};
+const std::string project_patch_version {"${PROJECT_VERSION_PATCH}"};
+
+
+#endif
