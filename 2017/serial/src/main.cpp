eb828b776dc4d8b817e87481812e273b218d9c91
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-24 16:41:22 +0900

    Change std::fstream to wiringSerial functions

--- 2017/serial/src/main.cpp
+++ 2017/serial/src/main.cpp
@@ -1,49 +1,34 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <system_error>
 #include <utility>
 #include <vector>
+
+#include <wiringSerial.h>
 
 
 int main(int argc, char** argv)
 {
-  std::ifstream file {"/home/yamasa/works/tmp/dummy_output.txt"};
-  std::vector<std::vector<unsigned long>> text_buffer {};
+  auto fd = serialOpen("/dev/ttyACM0", 9600);
 
-  if (file.is_open())
+  if (fd == -1)
   {
-    for (std::string word_buffer {}; std::getline(file, word_buffer); )
-    {
-      std::vector<unsigned long> line_buffer {};
-
-      for (std::stringstream ss {word_buffer}; std::getline(ss, word_buffer, ' '); )
-      {
-        word_buffer.erase(word_buffer.begin(), word_buffer.begin() + 2);
-        line_buffer.push_back(std::stoi(word_buffer));
-      }
-
-      text_buffer.emplace_back(std::move(line_buffer));
-    }
-
-    file.close();
-  }
-
-  else {
-    std::cerr << "[error] failed to open file\n";
+    std::error_code error {errno, std::generic_category()};
+    std::cerr << "[error] wiringSerial(3) - " << error.message() << std::endl;
     std::exit(EXIT_FAILURE);
   }
 
-
-  for (const auto& line : text_buffer)
+  while (true)
   {
-    for (const auto& word : line)
-    {
-      std::cout << word << (&word != &line.back() ? ' ' : '\n');
-    }
-  }
+    static char buffer {};
 
-  std::cout << "[debug] finish\n";
+    std::cout << "[debug] input: ";
+    std::cin >> buffer;
+
+    serialPutchar(fd, buffer);
+
+    std::cout << "[debug] return: " << serialGetchar(fd) << std::endl;
+  }
 
   return 0;
 }
