c8bdd0b12ca2d17c4ca7eeffe998ae613388859f
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-25 21:05:10 +0900

    Move template class "color_range" to file "color_range.hpp"

--- 2017/camera/src/main.cpp
+++ 2017/camera/src/main.cpp
@@ -2,37 +2,15 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <utility>
 
 #include <raspicam/raspicam_cv.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include <camera/version.hpp>
+#include <camera/color_range.hpp>
 
 
 namespace robocar {
-
-
-template <typename T>
-class color_range
-  : public std::pair<T,T>
-{
-public:
-  template <typename... Ts>
-  color_range(Ts&&... args)
-    : std::pair<T,T> {std::forward<Ts>(args)...}
-  {}
-
-  T min() const noexcept
-  {
-    return (*this).first;
-  }
-
-  T max() const noexcept
-  {
-    return (*this).second;
-  }
-};
 
 
 class camera
