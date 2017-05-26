5e0f15abe0dcd73c48fee511ea5b5d1123a61668
shimoe <shimoewithranobe@gmail.com>
2017-05-26 16:59:37 +0900

    [modified] fusion.ino

--- 2017/arduino_sketch/FUSION/fusion.ino
+++ 2017/arduino_sketch/FUSION/fusion.ino
@@ -2,9 +2,6 @@
 #include<VL6180X.h>
 #include<L3GD20.h>
 
-
-
-
 VL6180X vl6180x_1;
 VL6180X vl6180x_2;
 VL6180X vl6180x_3;
@@ -17,10 +14,20 @@ void setup(){
   Wire.begin();
 
   vl6180x_1.init();
-  vl6180x_1.configureDefault();
+  vl6180x_1.configureDefault(); //SLAVE_ADDRESS 0x212
   vl6180x_1.setTimeout(500);
 
-  if(!l3gd20.begin(l3gd20.L3GD20_RANGE_250DPS)){
+  vl6180x_2.init();
+  vl6180x_2.configureDefault(); 
+  vl6180x_2.setAddress(0x213);  //SLAVE_ADDRESS 0x213
+  vl6180x_2.setTimeout(500);
+
+  vl6180x_3.init();
+  vl6180x_3.configureDefault(); 
+  vl6180x_3.setAddress(0x214);  //SLAVE_ADDRESS 0x213
+  vl6180x_3.setTimeout(500);
+
+  if(!l3gd20.begin(l3gd20.L3GD20_RANGE_250DPS)){ //SLAVE_ADDRESS 0x6A
     Serial.println("Unable to initialize L3GD20");
     while(true);
   }
@@ -30,15 +37,18 @@ void setup(){
 void loop(){
   l3gd20.read();
 
-  Serial.print("VL6180X_1: ");  Serial.print(vl6180x_1.readRangeSingleMillimeters());
+  Serial.print("VL6180X_1: ");  Serial.print(vl6180x_1.readRangeSingleMillimeters()); Serial.println();
   if (vl6180x_1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-  Serial.println();
+  Serial.print("VL6180X_2: ");  Serial.print(vl6180x_2.readRangeSingleMillimeters()); Serial.println();
+  if (vl6180x_1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  Serial.print("VL6180X_3: ");  Serial.print(vl6180x_3.readRangeSingleMillimeters()); Serial.println();
+  if (vl6180x_1.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
 
   Serial.print("L3GD20_x: "); Serial.print((int) l3gd20.data.x); Serial.print(", ");
   Serial.print("L3GD20_y: "); Serial.print((int) l3gd20.data.y); Serial.print(", "); 
   Serial.print("L3GD20_z: "); Serial.println((int) l3gd20.data.z); Serial.println();
-g
+  
   int gp2y0a21yk = analogRead(0);
   Serial.print("GP2Y0A21YK: "); Serial.println(gp2y0a21yk);
 
