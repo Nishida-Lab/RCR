3d05ab7e020f617e012e37b5eee6e793a42242c6
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-28 20:11:17 +0900

    Update to include "robocar/serial" library

--- 2017/robocar/src/main.cpp
+++ 2017/robocar/src/main.cpp
@@ -2,9 +2,14 @@
 
 #include <boost/numeric/ublas/vector.hpp>
 
+#include <robocar/serial/wiring_serial.hpp>
+
+
 int main(int argc, char** argv)
 {
   boost::numeric::ublas::vector<double> direction {};
+
+  robocar::wiring_serial serial {"/dev/ttyACM0", 115200};
 
   return 0;
 }
