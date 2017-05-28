2184b40ec376965e98f0ef73078b2174e28bf987
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-28 20:23:51 +0900

    Add sensor_codes
    抽象層で呼んでるセンサの名前と，arduino側でのセンサの名前の変換用

--- 2017/robocar/src/main.cpp
+++ 2017/robocar/src/main.cpp
@@ -1,8 +1,29 @@
+#include <cstdint>
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 #include <boost/numeric/ublas/vector.hpp>
 
 #include <robocar/serial/wiring_serial.hpp>
+
+
+const std::unordered_map<std::string,std::int8_t> sensor_codes {
+  // {"",  0},
+  // {"",  1},
+  // {"",  2},
+  // {"",  3},
+  // {"",  4},
+  // {"",  5},
+  // {"",  6},
+  // {"",  7},
+  // {"",  8},
+  // {"",  9},
+  {"short_range_0", 10},
+  {"short_range_1", 11},
+  {"short_range_2", 12},
+  // {"", 13}
+};
 
 
 int main(int argc, char** argv)
