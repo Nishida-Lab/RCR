69cadc833a9756e3a72cebe60aebf085aba2aa58
YamaCIR-KIT <httperror@404-notfound.jp>
2017-05-30 16:26:01 +0900

    Cleanup

--- 2017/driver/include/driver/driver.hpp
+++ 2017/driver/include/driver/driver.hpp
@@ -38,12 +38,6 @@ public:
   }
 
 public:
-  // void write(const std::pair<int,int>& value) noexcept
-  // {
-  //   softPwmWrite(pin_.first,  value.first);
-  //   softPwmWrite(pin_.second, value.second);
-  // }
-
   template <typename T>
   void debug(const robocar::vector<T>& v, T tread) const
   {
