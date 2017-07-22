04a02c877717de3b699ddbb9e0587719dfc57ae5
YamaCIR-KIT <httperror@404-notfound.jp>
2017-07-22 20:00:28 +0900

    Update option parser

--- 2017/debugger/src/main.cpp
+++ 2017/debugger/src/main.cpp
@@ -13,19 +13,48 @@ int main(int argc, char** argv)
 {
   const std::vector<std::string> argv_ {argv, argv + argc};
 
-  if (1 < argv_.size())
+
+  for (auto iter {argv_.begin() + 1}; iter != argv_.end(); ++iter) [&]()
   {
-    for (auto iter {std::begin(argv_)}; iter != std::end(argv_); ++iter)
+    for (const auto& s : decltype(argv_) {"^-h$", "^--help$"})
     {
-      for (const auto& s : decltype(argv_) {"^-v$", "^--version$"})
+      if (std::regex_match(*iter, std::regex {s}))
       {
-        if (std::regex_match(*iter, std::regex {s}))
-        {
-          return !(std::cout << "version " << project_version.data() << " alpha\n").good();
-        }
+        std::cout << "rather, please help me\n";
+        return;
       }
     }
-  }
+
+    for (const auto& s : decltype(argv_) {"^-v$", "^--version$"})
+    {
+      if (std::regex_match(*iter, std::regex {s}))
+      {
+        std::cout << "version " << project_version.data() << " alpha\n";
+        return;
+      }
+    }
+
+    for (const auto& s : decltype(argv_) {"^-f$", "^--file$"})
+    {
+      if (std::regex_match(*iter, std::regex {s}))
+      {
+        if (++iter != argv_.end())
+        {
+          std::cout << "[debug] file: " << *iter << std::endl;
+        }
+
+        else
+        {
+          std::cerr << "[error] invalid argument\n";
+          std::exit(1);
+        }
+
+        return;
+      }
+    }
+
+    std::cerr << "[error] unknown option \"" << *iter << "\"\n";
+  }();
 
   return 0;
 }
