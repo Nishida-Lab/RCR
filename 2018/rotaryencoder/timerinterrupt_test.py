014e7f226f529a5072a04b4450be5d19556a6299
AtsukiYokota <atsuki.yokota@gmail.com>
2018-05-16 06:14:55 +0900

    Add timerinterrupt_test.py
    To test calculation angular velocity by timer interrupt

---
+++ 2018/rotaryencoder/timerinterrupt_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import signal
+import time
+import pigpio
+
+SAMPLING_TIME = 0.1
+ENCODER_PIN = 23
+
+count = 0
+velocity = 0
+
+def calc_vel(arg1, arg2):
+    global count
+    global velocity
+
+    velocity = (count * (360 / 500)) / SAMPLING_TIME
+    count = 0
+
+def count_pulse():
+    global count
+    count += 1
+
+signal.signal(signal.SIGALRM, calc_vel)
+signal.setitimer(signal.ITIMER_REAL, SAMPLING_TIME, SAMPLING_TIME)
+
+pi = pigpio.pi()
+pi.set_mode(ENCODER_PIN, pigpio.INPUT)
+pi.callback(ENCODER_PIN, pigpio.RISING_EDGE, count_pulse)
+
+while True:
+    print(velocity)
