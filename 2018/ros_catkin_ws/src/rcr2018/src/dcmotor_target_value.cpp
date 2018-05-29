5ae1ba23001cf66a60b3ccb4f06f969e462a8eb8
AtsukiYokota <atsuki.yokota@gmail.com>
2018-05-30 06:05:26 +0900

    Bug fix

--- 2018/ros_catkin_ws/src/rcr2018/src/dcmotor_target_value.cpp
+++ 2018/ros_catkin_ws/src/rcr2018/src/dcmotor_target_value.cpp
@@ -6,35 +6,8 @@
 #include <rcr2018/DcmCommand.h>
 #include <rcr2018/LineCount.h>
 
-ros::init(argc, argv, "dcmotor_target_value"); //ノード名の初期化
-
-ros::NodeHandle n; //パブリッシャのノードハンドル宣言
-
-ros::Publisher dcmotor_command_pub = n.advertise<rcr2018::DcmCommand>("dcm_command", 1); //パブリッシャの設定
-
-rcr2018::DcmCommand dcm;
-
 bool is_finish = false;
 const int dcm_pin = 18;
-
-//TofFrontメッセージを受信したとき動作する関数
-void tofmsgCallback(const rcr2018::TofFront::ConstPtr& msg)
-{
-  double target_value = 0.0; //目標値の初期化
-  if (is_finish)
-  {
-  target_value = 0.0;
-  }
-  else
-  {
-    target_value = 1 * std::tanh(msg->front); //目標角速度の決定
-  }
-
-  dcm.cmd_vel = target_value; //目標角速度をメッセージに代入
-
-  dcmotor_command_pub.publish(dcm); //パブリッシュ
-
-}
 
 //linecount callback function
 void linemsgCallback(const rcr2018::LineCount::ConstPtr& msg)
@@ -50,13 +23,37 @@ int main(int argc, char** argv)
 {
   const int frequency = 100;
 
-  //ros::init(argc, argv, "dcmotor_target_value"); //ノード名の初期化
+  ros::init(argc, argv, "dcmotor_target_value"); //ノード名の初期化
 
   ros::NodeHandle nh; //ノードハンドル宣言
 
-  ros::Subscriber dcmotor_command_sub = nh.subscribe("tof_front", 1, tofmsgCallback); //tofサブスクライバの設定
+  ros::Publisher dcmotor_command_pub = nh.advertise<rcr2018::DcmCommand>("dcm_command", 1); //パブリッシャの設定
 
-  ros::Subscriber line_count_sub = nh.subscribe("line_count", 1, linemsgCallback); //linecountサブスクライバの設定
+  rcr2018::DcmCommand dcm;
+
+  ros::Subscriber dcmotor_command_sub {nh.subscribe("tof_front", 1, 
+    std::function<void (const rcr2018::TofFront::ConstPtr&)>
+    {
+      [&](const rcr2018::TofFront::ConstPtr& constptr)
+      {
+        double target_value = 0.0; //目標値の初期化
+        if (is_finish)
+        {
+        target_value = 0.0;
+        }
+        else
+        {
+          target_value = 1 * std::tanh(constptr->front); //目標角速度の決定
+        }
+
+        constptr.cmd_vel = target_value; //目標角速度をメッセージに代入
+
+        dcmotor_command_pub.publish(dcm); //パブリッシュ
+      }
+    }
+  )}; //tofサブスクライバの設定
+
+  ros::Subscriber line_count_sub {nh.subscribe("line_count", 1, linemsgCallback)}; //linecountサブスクライバの設定
 
   ros::spin();
 
--- 2018/ros_catkin_ws/src/rcr2018/src/svmotor_target_value.cpp
+++ 2018/ros_catkin_ws/src/rcr2018/src/svmotor_target_value.cpp
@@ -7,28 +7,6 @@
 
 const int PWMPIN_S = 19; //PWMピンのピン配置を19番ピンに
 
-ros::NodeHandle n; //パブリッシャのノードハンドル宣言
-
-ros::Publisher svmotor_command_pub = n.advertise<rcr2018::SvmCommand>("svm_command", 1); //パブリッシャの設定
-
-rcr2018::SvmCommand svm;
-
-//メッセージを受信したとき動作する関数
-void msgCallback(const rcr2018::TofSide::ConstPtr& msg)
-{
-  double target_value = 0; //目標角度の初期化
-
-  double difference = msg->left - msg->right; //左センサと右センサの値の差
-
-  target_value = 0.6 * std::tanh(difference); //目標角度の決定
-
-  int input_pwm_value = target_value; //入力PWM信号のデューティ比を決定
-  // pwmWrite(PWMPIN_S, input_pwm_value); //サーボモータに出力
-  svm.cmd_ang_vel = target_value;
-  svmotor_command_pub.publish(svm);
-
-}
-
 int main(int argc, char** argv)
 {
   if (wiringPiSetupGpio() == -1)
@@ -43,7 +21,28 @@ int main(int argc, char** argv)
 
   ros::NodeHandle nh; //ノードハンドル宣言
 
-  ros::Subscriber svmotor_command_sub = nh.subscribe("tof_side", 1, msgCallback); //サブスクライバの設定
+  ros::Publisher svmotor_command_pub {n.advertise<rcr2018::SvmCommand>("svm_command", 1)}; //パブリッシャの設定
+
+  rcr2018::SvmCommand svm;
+
+  ros::Subscriber svmotor_command_sub {nh.subscribe("tof_side", 1, 
+    std::function<void (const rcr2018::TofSide::ConstPtr&)>
+    {
+      [&](const rcr2018::TofSide::ConstPtr& constptr)
+      {
+        double target_value = 0; //目標角度の初期化
+
+        double difference = constptr->left - constptr->right; //左センサと右センサの値の差
+
+        target_value = 0.6 * std::tanh(difference); //目標角度の決定
+
+        int input_pwm_value = target_value; //入力PWM信号のデューティ比を決定
+        // pwmWrite(PWMPIN_S, input_pwm_value); //サーボモータに出力
+        svm.cmd_ang_vel = target_value;
+        svmotor_command_pub.publish(svm);
+      }
+    }
+  )}; //サブスクライバの設定
 
   ros::spin();
 
