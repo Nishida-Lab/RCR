ad218428a2d3834c011eac3b806c178da7bf177c
AtsukiYokota <atsuki.yokota@gmail.com>
2018-06-19 20:59:29 +0900

    Modify photosensor.cpp for more robust

--- 2018/ros_catkin_ws/src/rcr2018/src/photosensor.cpp
+++ 2018/ros_catkin_ws/src/rcr2018/src/photosensor.cpp
@@ -20,19 +20,35 @@ const unsigned int thresh_color = 1000;
 const unsigned int thresh_read_count = 10;
 // コントロールライン検出後のスリープ時間[s]
 const unsigned int sleep_sec = 5;
+// スタートスイッチピン番号
+const int START_SW_PIN {25};
+// ループカウント数
+const int loop_count {100};
 
 int main(int argc, char** argv)
 {
+
+  int pin_value {0};
+
   if(wiringPiSetupGpio() == -1)
   {
     std::cout << "ERROR:Can't setup GPIO." << std::endl;
     return -1;
   }
 
+  pinMode(START_SW_PIN, INPUT);
+
   pullUpDnControl(GPIO_PIN, PUD_UP);
+
+  pin_value = digitalRead(START_SW_PIN);
+  while(pin_value != 0)
+  {
+    pin_value = digitalRead(START_SW_PIN);
+  }
 
   unsigned int pulse_width {0};
   unsigned int read_count {0};
+  unsigned long sum {0};
 
   //ROS周りのセットアップ
   ros::init(argc, argv, "photosensor");
@@ -44,14 +60,21 @@ int main(int argc, char** argv)
   while (ros::ok())
   {
     pulse_width = 0;
+    sum = 0;
 
-    sensor_charge();
-
-    while (digitalRead(GPIO_PIN) == 1)
+    for (int i = 0; i < loop_count; i++)
     {
-      ++pulse_width;
+      sensor_charge();
+
+      while (digitalRead(GPIO_PIN) == 1)
+      {
+        ++pulse_width;
+      }
+      sum += pulse_width;
+      pulse_width = 0;
     }
 
+    pulse_width = static_cast<unsigned int>(sum / loop_count);
     // 白色の場合
     if (pulse_width < thresh_color)
     {
