d13b7fed854706062f55ca007dc3c2cffb4448ae
AtsukiYokota <atsuki.yokota@gmail.com>
2018-06-20 19:09:15 +0900

    Done pi control experiment

--- 2018/ros_catkin_ws/src/rcr2018/src/driver_test.cpp
+++ 2018/ros_catkin_ws/src/rcr2018/src/driver_test.cpp
@@ -4,8 +4,8 @@
 #include <ros/ros.h>
 #include <rcr2018/AngVel.h>
 
-const double kp {0.120}; //比例ゲインを決定
-const double ki {0.060}; //積分ゲインを決定
+const double kp {0.049}; //比例ゲインを決定
+const double ki {0.0049}; //積分ゲインを決定
 
 const int PWMPIN_D {18}; //PWMピンのピン配置を18番ピンに
 const int DIRPIN {23}; //DIRピンのピン配置を23番ピンに
@@ -13,6 +13,7 @@ const int frequency {500}; //DCモータの周波数
 const double target_value {150.0}; //目標角速度
 
 double ang_vel {0.0}; //出力角度の初期化
+double input_value_pre {0.0};
 double output_value_pre {0.0}; //一つ前時点での出力角速度
 double input_pwm_value_pre {0.0}; //一つ前時点での入力PWM信号
 double dev_tar_out {0.0}; //現時点での目標値と出力値の差の初期化
@@ -32,13 +33,23 @@ void encodermsgCallback(const rcr2018::AngVel::ConstPtr& msg)
 
     dev_tar_out = target_value - output_value; //目標角速度と出力角度の差分
 
-    double input_value {(kp * (dev_tar_out - dev_tar_out_pre)) + (ki * dev_tar_out)}; //PI制御器による入力値の決定
+    double control_value {(kp * (dev_tar_out - dev_tar_out_pre)) + (ki * dev_tar_out)}; //PI制御器による入力値の決定
+    
+    double input_value {input_value_pre + control_value};
 
     double input_pwm_value {2240.1 * input_value + 29594.0}; //入力PWM信号のデューティ比を決定
+    
+    if (input_pwm_value > 1000000)
+    {
+       input_pwm_value = 1000000;
+       std::cout << "Divergence" << std::endl;
+    }
 
     gpioWrite(DIRPIN, 0); //DIRピンの出力を決定
     gpioHardwarePWM(PWMPIN_D, frequency, static_cast<int>(input_pwm_value)); //DCモータにPWM信号を入力
-    imput_pwm_value_pre = input_pwm_value; //現時点での入力PWM信号を保存
+
+    input_value_pre = input_value;
+    input_pwm_value_pre = input_pwm_value; //現時点での入力PWM信号を保存
     output_value_pre = output_value; //現時点での出力角速度を保存
     dev_tar_out_pre = dev_tar_out; //次の時点のため、現時点での値を保存
   }
