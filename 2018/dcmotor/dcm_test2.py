7fb3b51ab0d66121f7163c022a5589882f5555c0
AtsukiYokota <atsuki.yokota@gmail.com>
2018-06-20 20:50:02 +0900

    Done pwm and ang_vel experiment

--- 2018/dcmotor/dcm_test2.py
+++ 2018/dcmotor/dcm_test2.py
@@ -12,10 +12,10 @@ pi1 = pigpio.pi()
 pi1.set_mode(PWMPIN, pigpio.OUTPUT)
 pi1.set_mode(DIRPIN, pigpio.OUTPUT)
 
-for DUTY_CYCLE in range(100000, 1000000, 100000):
+for DUTY_CYCLE in range(50000, 500001, 50000):
     pi1.write(DIRPIN, 0)
     pi1.hardware_PWM(PWMPIN, 500, DUTY_CYCLE)
-    time.sleep(5)
+    time.sleep(3)
 
 pi1.set_mode(PWMPIN, pigpio.INPUT)
 pi1.stop()
