51b0ad38f38cf27d3d218f65b8a0677c02cbf506
AtsukiYokota <atsuki.yokota@gmail.com>
2018-06-27 18:11:49 +0900

    Modify servo_test2.py
    To do stearing angular test

--- 2018/servo/servo_test2.py
+++ 2018/servo/servo_test2.py
@@ -1,16 +1,32 @@
+from __future__ import division
 import pigpio
 import time
 
+def calc_ang(i):
+    return (-100 * (i - 1.5))
+
+
 PWM_PIN =  19
 frequency = 50
+range_list = [0.1 * x for x in range(9, 22)]
+input_ang = []
+servo_ang = []
 
 pi = pigpio.pi()
 pi.set_mode(PWM_PIN, pigpio.OUTPUT)
 
-for i in range(0.9,2.2,0.1):
+for i in range_list:
     duty = 1000000 * i / 20
     pi.hardware_PWM(PWM_PIN, frequency, duty)
-    time.sleep(5)
+    print(calc_ang(i))
+    servo_ang.append(calc_ang(i))
+    
+    value = raw_input('input measurement ang')
+    input_ang.append(value)
+
+print('servo_ang:input_ang')
+for s, i in zip(servo_ang, input_ang):
+    print('{}:{}'.format(s, i))
 
 pi.set_mode(PWM_PIN, pigpio.INPUT)
 pi.stop()
